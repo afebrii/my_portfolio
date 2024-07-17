1403d10632f000b2f0dc853618f018a8dc88f5db
andika febriansyah <82563655+afebrii@users.noreply.github.com>
2024-07-17 10:06:23 +0700

    add experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Expreriences from "./components/Expreriences";
 
 const App = () => {
   return (
-    <div className="overflow-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full">
+    <div className="overflow-hidden antialiased text-neutral-300 selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed top-0 w-full h-full -z-10">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
-      <div className="container mx-auto px-8">
+      <div className="container px-8 mx-auto">
         <Navbar />
         <Hero />
         <About />
         <Technologies />
+        <Expreriences />
       </div>
     </div>
   );
