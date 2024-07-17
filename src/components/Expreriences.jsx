f39ba09f8dfbc963d8db77ed686363e3ecbb90ce
andika febriansyah <82563655+afebrii@users.noreply.github.com>
2024-07-17 19:38:18 +0700

    add animations

--- src/components/Expreriences.jsx
+++ src/components/Expreriences.jsx
@@ -1,15 +1,28 @@
 import { EXPERIENCES } from "../constants";
+import { motion } from "framer-motion";
 
 const Expreriences = () => {
   return (
     <div className="pb-4 border-b border-neutral-900">
-      <h2 className="my-20 text-4xl text-center">Experience</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-4xl text-center"
+      >
+        Experience
+      </motion.h2>
       <div>
         {EXPERIENCES.map((experience, index) => (
           <div key={index} className="flex flex-wrap mb-8 lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
               <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
-            </div>
+            </motion.div>
             <div className="w-full max-w-xl lg:w-3/4">
               <h6 className="mb-2 font-semibold">
                 {experience.role} -{" "}
